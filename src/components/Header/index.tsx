0f46a5cd6a2959aa7ab39abe7e8a42a088762be1
Jonatas Santos <jonatassantos96@hotmail.com>
2021-06-17 08:20:25 -0300

    🚧 Workin in Header and fixed bug of the import icons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
-import {Container} from './styles';
+import {Container, Top, Logo, Title} from './styles';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import logo from '../../assets/Nubank_Logo.png';
 
 function Header() {
-  return <Container />;
+  return (
+    <Container>
+      <Top>
+        <Logo source={logo} />
+        <Title>Jônatas</Title>
+      </Top>
+      <Icon name="keyboard-arrow-down" size={20} color={'#FFF'} />
+    </Container>
+  );
 }
 
 export default Header;
