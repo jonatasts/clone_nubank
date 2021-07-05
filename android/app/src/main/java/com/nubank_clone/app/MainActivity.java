15ff2c04e2673d4cda5b25cf096aa82ff09a54e2
Jonatas Santos <jonatassantos96@hotmail.com>
2021-07-05 11:53:19 -0300

    ✨ Renamed name app

--- android/app/src/main/java/com/nubank_clone/app/MainActivity.java
+++ android/app/src/main/java/com/nubank_clone/app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.nubankapp;
+package com.nubank_clone.app;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "nubankApp";
+    return "Nubank";
   }
 
   @Override
--- ios/NubankTests/NubankTests.m
+++ ios/NubankTests/NubankTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface nubankAppTests : XCTestCase
+@interface NubankTests : XCTestCase
 
 @end
 
-@implementation nubankAppTests
+@implementation NubankTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
